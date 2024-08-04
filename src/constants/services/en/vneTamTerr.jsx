4100ccd82efde36599ae90620212dbd2e59b8301
have2go <boxadmail@yandex.ru>
2024-08-04 18:31:03 +0300

    added 2 missing services (both ru & eng versions)

--- src/constants/services/en/vneTamTerr.jsx
+++ src/constants/services/en/vneTamTerr.jsx
@@ -1,9 +1,22 @@
 export const vneTamTerr = {
-    title: "Processing outside the customs territory",
-    text: () => (
-        <div className="flex flex-col text-left gap-5 SM:text-sm">
-            <p className="">–</p>
-        </div>
-    ),
-    type: "vivoz",
+  title: "Processing outside the customs territory",
+  text: () => (
+    <div className="flex flex-col text-left gap-5 SM:text-sm">
+      <p className="">
+        Processing outside the customs territory is a customs procedure,
+        applicable to goods of the Union that are exported from the customs
+        territory territories of the Union for processing operations in order to
+        receipt of products of their processing, subsequently imported to the
+        customs the territory of the Union, without payment of export taxes in
+        respect of such goods of the Union customs duties.
+        <br />
+        Conditions of placement of goods – availability of a document on the
+        conditions of processing, the possibility of identifying Union goods in
+        their processed products, compliance with prohibitions and restrictions,
+        subject to the conditions of the premises goods under this customs
+        procedure and their use in accordance with such a customs procedure;{" "}
+      </p>
+    </div>
+  ),
+  type: "vivoz",
 };
--- src/constants/services/en/vremVivoz.jsx
+++ src/constants/services/en/vremVivoz.jsx
@@ -1,9 +1,17 @@
 export const vremVivoz = {
-    title: "Temporary export",
-    text: () => (
-        <div className="flex flex-col text-left gap-5 SM:text-sm">
-            <p className="">-</p>
-        </div>
-    ),
-    type: "vivoz",
+  title: "Temporary export",
+  text: () => (
+    <div className="flex flex-col text-left gap-5 SM:text-sm">
+      <p className="">
+        Temporary export is a customs procedure applied to goods When goods are
+        exported from the customs territory of the Union for their temporary
+        stay and use outside its borders without payment export customs duties.
+        <br />
+        Conditions for the placement of goods – the possibility of identifying
+        goods when they are subsequently placed under the customs procedure in
+        order to complete actions of the customs procedure of temporary export
+      </p>
+    </div>
+  ),
+  type: "vivoz",
 };
